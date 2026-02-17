5f0be9ff552e272a244807d4fffddcb12ef1c613
Joshua Still <Joshua_Still@outlook.com>
2026-02-17 15:19:38 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js", // Renamed to avoid confusion with src/index.js
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html", // This points to your skeleton!
+    }),
+  ],
+  devServer: {
+    watchFiles: ["./src/template.html"], // Forces a reload when you edit HTML
+  },
+};
