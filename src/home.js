987345d9086076adc32ab350531ee5ae76d3982b
Joshua Still <Joshua_Still@outlook.com>
2026-03-24 16:26:56 -0600

    styling to home page

--- src/home.js
+++ src/home.js
@@ -1,13 +1,25 @@
+import ufoLandingImg from './assets/alien_landing.png';
+
 export function loadHome() {
     console.log('home loaded')
     const content = document.getElementById("content");
 
     // Clear content first
     content.innerHTML = "";
+    
 
     //home page wrapper (whole page)
     let homeWrapper = document.createElement('div');
     homeWrapper.classList.add('home-container');
+
+    homeWrapper.style.backgroundImage = `url(${ufoLandingImg})`;
+
+    homeWrapper.style.backgroundSize = "cover";
+    homeWrapper.style.backgroundPosition = "center";
+    homeWrapper.style.height = "100vh"; // Make sure it covers the screen
+
+
+
 
     //headline wrapper (top of page)
     let headlineWrapper = document.createElement('div');
@@ -19,20 +31,24 @@ export function loadHome() {
     headline.id = 'headline'
     headline.textContent = 'UFO Pizza'
 
-    //p tag
-    let slogan = document.createElement('p')
-    slogan.id = 'slogan'
-    slogan.textContent = 'pizza out of this world !'
+    //sub title tag
+    const subtitleTag = document.createElement('div');
+    subtitleTag.classList.add('subtitleTag');
+    const fluff = document.createElement('p');
+    fluff.textContent = 'TRANSMISSION STATUS: ENCRYPTED';
 
-    //img here
+
 
     // 3. Append the children to the WRAPPER
     headlineWrapper.appendChild(headline);
     homeWrapper.appendChild(headlineWrapper);
-    headlineWrapper.appendChild(slogan);
+    headlineWrapper.appendChild(subtitleTag);
+    subtitleTag.appendChild(fluff);
+
 
     // 4. Finally, append the WRAPPER to the main #content div
     content.appendChild(homeWrapper);
+    // alienHome.apppendChild(homeWrapper)
   
 }
 
